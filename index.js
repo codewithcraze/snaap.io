fd4a6671fe28c263b856670fb35c23586fed7ded
Deepak Chaudhary <iamdeepakchaudhary2000@gmail.com>
2024-12-04 22:51:00 +0530

    day-1

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3004;
+const bodyParser = require("body-parser");
+ 
+
+
+app.use(bodyParser.json());  // To parse application/json content
+
+// Middleware to parse URL-encoded request body (common for form submissions)
+app.use(bodyParser.urlencoded({ extended: true }));  // To parse application/x-www-form-urlencoded content
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,6 +29,29 @@ app.get('/terms-and-conditions', (req, res) => {
 
 app.get('/sitmap.xml', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'sitemap.xml'))
+})
+
+app.get('/contact-us', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'))
+})
+
+app.post('/submit-contact-form', (req, res) => {
+    try{
+        const {firstName, lastName, email, message, subscribe } = req.body;
+
+        res.json({
+            firstName,
+            lastName,
+            message,
+            email,
+            subscribe
+        })
+    }catch(error){
+        res.json({
+            status: false,
+            error: error.message
+        })
+    }
 })
 
 app.listen(port, () => {
