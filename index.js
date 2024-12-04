822741bb3b10a537e7b96d2891abec73b5658553
Deepak Chaudhary <iamdeepakchaudhary2000@gmail.com>
2024-12-04 20:57:38 +0530

    privacy policy and terms and condition add

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/privacy-policy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'privacy-policy.html'))
+})
+
+app.get('/terms-and-conditions', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'terms-and-condition.html'))
+})
+
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
